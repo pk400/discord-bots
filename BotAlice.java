0480a70ab24aa3d60808612e9c5826ccb6e404d4
pk400 <joelaro@outlook.com>
2015-10-09 01:24:46 -0400

    Added OMDB API features

--- BotAlice.java
+++ BotAlice.java
@@ -8,6 +8,8 @@ import java.util.Random;
 import java.util.Vector;
 
 import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.json.simple.parser.ParseException;
 
 import sx.blah.discord.obj.Channel;
@@ -17,25 +19,40 @@ import sx.blah.discord.obj.Message;
 import sx.blah.discord.obj.User;
 
 public class BotAlice extends DiscordClient {
-	Vector<String> mentionedStrings;
+	JsonReader reader;
+	Vector<String> mentionedStrings, answerQuestions;
 	Boolean playAccepted;
 	
 	public BotAlice(String email, String password) throws URISyntaxException, IOException, ParseException {
 		super(email, password);
 		// TODO Auto-generated constructor stub
-		System.out.println("Initializing Alice.");
+		System.out.println("Initializing Alice.");	
+
+		reader = new JsonReader();
 		
 		mentionedStrings = new Vector<String>();
 		mentionedStrings.add("**Yes master, I am here.:blue_heart: What is your command?**");
 		mentionedStrings.add("**I am awake. :)**");
 		mentionedStrings.add("**Who calls me?**");
 		mentionedStrings.add("**~yawn~**");
+
+		answerQuestions = new Vector<String>();
+		answerQuestions.add("**Absolutely!**");
+		answerQuestions.add("**Yes.**");
+		answerQuestions.add("**Most likely.**");
+		answerQuestions.add("**Maybe.**");
+		answerQuestions.add("**I am not going to answer that.**");
+		answerQuestions.add("**Not really.**");
+		answerQuestions.add("**No.**");
+		answerQuestions.add("**Absolutely not!**");
+		
 		
 		playAccepted = false;
 		
 		System.out.println("Alice is awake.");
 	}
-
+	
+	
 	@Override
 	public void connect() {
 		// TODO Auto-generated method stub
@@ -66,6 +83,22 @@ public class BotAlice extends DiscordClient {
 		super.onError(e);
 	}
 
+	private void getMovieInfo(String movie, Message m) throws JSONException, IOException {
+		JSONObject json = reader.readJsonFromUrl(movie);
+		StringBuilder sb = new StringBuilder();
+		try {
+			sb.append(json.get("Poster") + "\\n");
+			sb.append("**" + json.get("Title") + "**");
+			sb.append(" (" + json.get("Year") + ")");
+			sb.append("\\nRating: " + json.get("Rated"));
+			sb.append("\\nGenre(s): " + json.get("Genre"));
+			sb.append("\\nPlot: " + json.get("Plot"));
+			m.reply(sb.toString(), this);
+		} catch(IOException | ParseException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	public void onMessageReceive(Message message) {
 		// TODO Auto-generated method stub
@@ -78,8 +111,24 @@ public class BotAlice extends DiscordClient {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		} else if(message.getContent().contains("@dump")) {
+			try {
+				message.reply(message.getAuthor().getID(), this);
+				playAccepted = true;
+			} catch (IOException | ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else if(message.getContent().startsWith("@omdb")) {
+			try {
+				getMovieInfo("http://www.omdbapi.com/?t=" + message.getContent().substring(6), message);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
+
 
 	@Override
 	public void onMessageSend(Message message) {
@@ -92,7 +141,15 @@ public class BotAlice extends DiscordClient {
 		// TODO Auto-generated method stub
 		super.onMentioned(message);
 		
-		if(message.getContent().contains("wanna play a game?")) {
+		if(message.getContent().endsWith("?")) {
+			try{
+				Random gen = new Random();
+				int rand = gen.nextInt(answerQuestions.size());
+				message.reply((String)answerQuestions.get(rand), this);
+			} catch(IOException | ParseException e) {
+				e.printStackTrace();
+			}
+		} else if(message.getContent().contains("wanna play a game?")) {
 			try {
 				message.reply("**Sure! I love games! Let's play **@" + message.getAuthor().getName() + "**!**", this);
 				playAccepted = true;
