b51a6091f8b52e79ef88ad0a13784c36e69bbbad
pk400 <joelaro@outlook.com>
2015-10-11 16:06:32 -0400

    Updated Archie to new Discord4J

--- BotAlice.java
+++ BotAlice.java
@@ -1,148 +1,34 @@
 
+
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Random;
-import java.util.Vector;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.json.simple.parser.ParseException;
 
-import modules.JsonReader;
 import sx.blah.discord.DiscordClient;
-import sx.blah.discord.obj.Message;
+import sx.blah.discord.handle.IListener;
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
 
-public class BotAlice extends DiscordClient {
+public class BotAlice {
 	// TODO: move these to file on server later
 	private final static String botemail 	= "pk400@cock.li";
 	private final static String botpass 	= "abc123";
 	
-	JsonReader reader;
-	Vector<String> mentionedStrings, answerQuestions;
-	Boolean playAccepted;
-	
-	public BotAlice(String email, String password) throws URISyntaxException, IOException, ParseException {
-		super(botemail, botpass);
-		
-		reader = new JsonReader();
-		
-		mentionedStrings = new Vector<String>();
-		mentionedStrings.add("**Yes master, I am here.:blue_heart: What is your command?**");
-		mentionedStrings.add("**I am awake. :)**");
-		mentionedStrings.add("**Who calls me?**");
-		mentionedStrings.add("**~yawn~**");
-
-		answerQuestions = new Vector<String>();
-		answerQuestions.add("**Absolutely!**");
-		answerQuestions.add("**Yes.**");
-		answerQuestions.add("**Most likely.**");
-		answerQuestions.add("**Maybe.**");
-		answerQuestions.add("**I am not going to answer that.**");
-		answerQuestions.add("**Not really.**");
-		answerQuestions.add("**No.**");
-		answerQuestions.add("**Absolutely not!**");
-		
-		
-		playAccepted = false;
-		
-		System.out.println("Alice is online.");
-	}
-	
-	private void getMovieInfo(String movie, Message m) throws JSONException, IOException {
-		JSONObject json = reader.readJsonFromUrl(movie);
-		StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) {
 		try {
-			sb.append(json.get("Poster") + "\\n");
-			sb.append("**" + json.get("Title") + "**");
-			sb.append(" (" + json.get("Year") + ")");
-			sb.append("\\nRating: " + json.get("Rated"));
-			sb.append("\\nGenre(s): " + json.get("Genre"));
-			sb.append("\\nPlot: " + json.get("Plot"));
-			m.reply(sb.toString(), this);
-		} catch(IOException | ParseException e) {
+			DiscordClient.get().login(botemail, botpass);
+			
+			DiscordClient.get().getDispatcher().registerListener(new IListener<MessageReceivedEvent>() {
+
+				@Override
+				public void receive(MessageReceivedEvent event) {
+					
+				}
+				
+			});
+		} catch (IOException | ParseException | URISyntaxException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	@Override
-	public void onMessageReceive(Message message) {
-		// TODO Auto-generated method stub
-		super.onMessageReceive(message);
-		if(message.getContent().contains("Alice go play")) {
-			try {
-				message.reply("Alright. Hey @" + getUserByID("101513350944428032").getName() + ", Wanna Play?", this);
-				message.reply("@GoPlay", this);
-			} catch (IOException | ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} else if(message.getContent().contains("@dump")) {
-			try {
-				message.reply(message.getAuthor().getID(), this);
-				playAccepted = true;
-			} catch (IOException | ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} else if(message.getContent().startsWith("@omdb")) {
-			try {
-				getMovieInfo("http://www.omdbapi.com/?t=" + message.getContent().substring(6), message);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
 
-	@Override
-	public void onMentioned(Message message) {
-		// TODO Auto-generated method stub
-		super.onMentioned(message);
-		
-		if(message.getContent().endsWith("?")) {
-			try{
-				Random gen = new Random();
-				int rand = gen.nextInt(answerQuestions.size());
-				message.reply((String)answerQuestions.get(rand), this);
-			} catch(IOException | ParseException e) {
-				e.printStackTrace();
-			}
-		} else if(message.getContent().contains("wanna play a game?")) {
-			try {
-				message.reply("**Sure! I love games! Let's play **@" + message.getAuthor().getName() + "**!**", this);
-				playAccepted = true;
-			} catch (IOException | ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} else if(message.getContent().contains("@dump")) {
-			try {
-				message.reply(message.getAuthor().getID(), this);
-				playAccepted = true;
-			} catch (IOException | ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} /*else {
-			Random gen = new Random();
-			int rand = gen.nextInt(mentionedStrings.size());
-			
-			try {
-				message.reply((String)mentionedStrings.get(rand), this);
-			} catch (IOException | ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}*/
-	}
-	
-	
-	/*public static void main(String... args) {
-	    try {
-		    new BotAlice("pk400@cock.li", "abc123");
-	    } catch (Exception e) {
-		    e.printStackTrace();
-	    }
-	}*/
-	
 }
--- BotArchie.java
+++ BotArchie.java
@@ -1,90 +1,78 @@
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.time.format.DateTimeFormatter;
 
 import org.json.simple.parser.ParseException;
 
 import sx.blah.discord.DiscordClient;
-import sx.blah.discord.obj.Message;
+import sx.blah.discord.handle.IListener;
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
+import sx.blah.discord.handle.obj.Message;
 
-public class BotArchie extends DiscordClient {
+public class BotArchie {
 	// TODO: move these to file on server later
 	private final static String botemail 	= "botarchie400@cock.li";
 	private final static String botpass 	= "abc123";
-
-	StringBuilder history;
-	PrintWriter file;
 	
-	public BotArchie(String email, String password) throws URISyntaxException, IOException, ParseException {
-		//super(email, password);
-		super(botemail, botpass);
-		
-		history = new StringBuilder();
-		
-		System.out.println("Archie is online.");
-	}
-
-	@Override
-	public void connect() {
-		// TODO Auto-generated method stub
-		super.connect();
-	}
-
-	@Override
-	public void onMessageReceive(Message m) {
-		super.onMessageReceive(m);
-		
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM HH:mm:ss");
-		
-		/*history.append(m.getTimestamp().format(dtf) + " | "
-				+ " **" + m.getAuthor().getName() + "**"
-				+ ": *" + m.getContent() + "*\\n");*/
-		
-		StringBuilder hist = new StringBuilder();
-		hist.append(m.getTimestamp().format(dtf) + " | "
-				+ " **" + m.getAuthor().getName() + "**"
-				+ ": *" + m.getContent() + "*\\n");
-
-		BufferedWriter bw = null;
+	public static void main(String... args) {
 		try {
-			bw = new BufferedWriter(new FileWriter(m.getChannelID().toString() + ".log", true));
-			bw.write(hist.toString());
-			bw.newLine();
-			bw.flush();
-			if(bw != null)
-				bw.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		if(m.getContent().equals("/history")) {
-			BufferedReader br = null;
+			DiscordClient.get().login(botemail, botpass);
 			
-			try {
-				br = new BufferedReader(new FileReader(m.getChannelID().toString() + ".log"));
-				StringBuilder sb = new StringBuilder();
-				String line;
-				
-				sb.append("_**From " +m.getChannelID().toString() + "**_\\n\\n");
-				
-				while((line = br.readLine()) != null) {
-					sb.append(line);
+			DiscordClient.get().getDispatcher().registerListener(new IListener<MessageReceivedEvent>() {
+	
+				@Override
+				public void receive(MessageReceivedEvent event) {
+					Message m = event.getMessage();
+					
+					//---------------------------------------------------------------------------------------------------------------\\
+					// WRITE TO LOG
+					DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM HH:mm:ss");
+					
+					StringBuilder addlog = new StringBuilder();
+					addlog.append(m.getTimestamp().format(dtf) + " **" + m.getAuthor().getName() + "**: *" + m.getContent() + "*\\n");
+					
+					BufferedWriter bw = null;
+					try {
+						bw = new BufferedWriter(new FileWriter(m.getChannelID().toString() + ".log", true));
+						bw.write(addlog.toString());
+						bw.newLine();
+						bw.flush();
+						if(bw != null)
+							bw.close();
+					} catch(IOException e) {
+						e.printStackTrace();
+					}
+					//---------------------------------------------------------------------------------------------------------------//
+										
+					if(m.getContent().equals("/log")) {
+						BufferedReader br = null;
+						try {
+							br = new BufferedReader(new FileReader(m.getChannelID().toString() + ".log"));
+							StringBuilder sb = new StringBuilder();
+							String line;
+							
+							sb.append("_**From " + m.getChannelID().toString() + "**_\\n\\n");
+							
+							while((line = br.readLine()) != null) {
+								sb.append(line);
+							}
+							
+							br.close();
+							
+							m.reply(sb.toString());
+						} catch (IOException | ParseException e) {
+							e.printStackTrace();
+						}
+					}
 				}
-				
-				br.close();
-				
-				m.reply(sb.toString(), this);
-			} catch (IOException | ParseException e) {
-				e.printStackTrace();
-			}
+			});
+		} catch (IOException | ParseException | URISyntaxException e1) {
+			e1.printStackTrace();
 		}
 	}
 }
