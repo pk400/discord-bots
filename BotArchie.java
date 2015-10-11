3f75381a8f6f4b1668fa1c9084fa5fcd30d8b960
pk400 <joelaro@outlook.com>
2015-10-11 16:13:21 -0400

    Archie formatting

--- BotArchie.java
+++ BotArchie.java
@@ -19,60 +19,64 @@ public class BotArchie {
 	private final static String botemail 	= "botarchie400@cock.li";
 	private final static String botpass 	= "abc123";
 	
-	public static void main(String... args) {
-		try {
-			DiscordClient.get().login(botemail, botpass);
-			
-			DiscordClient.get().getDispatcher().registerListener(new IListener<MessageReceivedEvent>() {
-	
-				@Override
-				public void receive(MessageReceivedEvent event) {
-					Message m = event.getMessage();
-					
-					//---------------------------------------------------------------------------------------------------------------\\
-					// WRITE TO LOG
-					DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM HH:mm:ss");
-					
-					StringBuilder addlog = new StringBuilder();
-					addlog.append(m.getTimestamp().format(dtf) + " **" + m.getAuthor().getName() + "**: *" + m.getContent() + "*\\n");
-					
-					BufferedWriter bw = null;
+	public static void main(String... args) 
+			throws IOException, ParseException, URISyntaxException {
+		
+		DiscordClient.get().login(botemail, botpass);
+		
+		DiscordClient.get().getDispatcher().registerListener(
+				new IListener<MessageReceivedEvent>() {
+
+			@Override
+			public void receive(MessageReceivedEvent event) {
+				Message m = event.getMessage();
+
+				//------------------------------------------------------------//
+				// WRITE TO LOG
+				DateTimeFormatter dtf = 
+						DateTimeFormatter.ofPattern("dd/MM HH:mm:ss");
+				
+				StringBuilder addlog = new StringBuilder();
+				addlog.append(m.getTimestamp().format(dtf) 
+						+ " **" + m.getAuthor().getName() 
+						+ "**: *" + m.getContent() + "*\\n");
+				
+				BufferedWriter bw = null;
+				try {
+					bw = new BufferedWriter(
+							new FileWriter(m.getChannelID().toString() + ".log", true));
+					bw.write(addlog.toString());
+					bw.newLine();
+					bw.flush();
+					if(bw != null)
+						bw.close();
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+				//------------------------------------------------------------//
+									
+				if(m.getContent().equals("/log")) {
+					BufferedReader br = null;
 					try {
-						bw = new BufferedWriter(new FileWriter(m.getChannelID().toString() + ".log", true));
-						bw.write(addlog.toString());
-						bw.newLine();
-						bw.flush();
-						if(bw != null)
-							bw.close();
-					} catch(IOException e) {
+						br = new BufferedReader(
+								new FileReader(m.getChannelID().toString() + ".log"));
+						StringBuilder sb = new StringBuilder();
+						String line;
+						
+						sb.append("_**From " + m.getChannelID().toString() + "**_\\n\\n");
+						
+						while((line = br.readLine()) != null) {
+							sb.append(line);
+						}
+						
+						br.close();
+						
+						m.reply(sb.toString());
+					} catch (IOException | ParseException e) {
 						e.printStackTrace();
 					}
-					//---------------------------------------------------------------------------------------------------------------//
-										
-					if(m.getContent().equals("/log")) {
-						BufferedReader br = null;
-						try {
-							br = new BufferedReader(new FileReader(m.getChannelID().toString() + ".log"));
-							StringBuilder sb = new StringBuilder();
-							String line;
-							
-							sb.append("_**From " + m.getChannelID().toString() + "**_\\n\\n");
-							
-							while((line = br.readLine()) != null) {
-								sb.append(line);
-							}
-							
-							br.close();
-							
-							m.reply(sb.toString());
-						} catch (IOException | ParseException e) {
-							e.printStackTrace();
-						}
-					}
 				}
-			});
-		} catch (IOException | ParseException | URISyntaxException e1) {
-			e1.printStackTrace();
-		}
+			}
+		});
 	}
 }
